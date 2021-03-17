f951e279bdabfe8a1d8d0249c3b76db6e33eeae3
Earl Dukerschein <47160211+X01XX@users.noreply.github.com>
2021-03-17 13:46:07 -0500

    Add files via upload
    Added info in display of storage used by array, overhead and total.

--- mm_array.fs
+++ mm_array.fs
@@ -3,24 +3,44 @@
 \ Create and manage an array, where:
 \   Allocation involves popping an array element address from a stack,
 \   Deallocation involves pushing an array element address onto the stack.
+\
+\   Memory is arranged as:
+\
+\     Stack
+\       one cell for info, capacity and current number free
+\       one cell for each array item
+\
+\     Array
+\       one cell for stack address
+\       one cell for item size in bytes
+\       items
 
 include stack.fs 
-cr
-.( mma-deallocate: Run like: <item-addr> <mma-addr> mma-deallocate )
+
+' @ alias _mm-get-stack ( mma-addr -- stack-addr )
+' ! alias _mm-set-stack ( stack-addr mma-addr -- )
+
+: _mm-set-item-size ( item-size mma-addr -- )
+    cell + !
+;
+
+: _mm-get-item-size ( mma-addr -- item-size )
+    cell + @
+;
+
+\ ( mma-deallocate: Run like: <item-addr> <mma-addr> mma-deallocate )
 : mma-deallocate ( item-addr mma-addr -- )
-	@		\ item-addr stack-addr
+	_mm-get-stack	\ item-addr stack-addr
         over swap	\ item-addr item-addr stack-addr
 	stack-push	\ item-addr
         0 swap !	\ ( zero out first cell of deallocated item )
 ;
-cr
-.( mma-allocate: Run like: <mma-addr> mma-allocate -- array-item-addr )
+\ ( mma-allocate: Run like: <mma-addr> mma-allocate -- array-item-addr )
 : mma-allocate ( mma-addr -- item-addr )
-	@		\ stack-addr
+	_mm-get-stack	\ stack-addr
 	stack-pop	\ item-addr
 ;
-cr
-.( mma-new.  Run like: <num-cells-per-item> <num-items> mma-new value <mma-name>.
+\ ( mma-new.  Run like: <num-cells-per-item> <num-items> mma-new value <mma-name>.
 : mma-new ( num-cells-per-item  num-items -- mma-addr )
 
         swap		\ n-i n-c-p-i
@@ -31,7 +51,7 @@ cr
         swap		\ n-i item-size n-i
         over		\ n-i item-size n-i item-size
         *		\ n-i item-size all-items-size
-        cell +		\ n-i item-size total-size ( add one word for the stack address )
+        2 cells +	\ n-i item-size total-size ( add two cells for the stack address, item size )
 
         cfalign
 	here		\ n-i item-size total-size free-address
@@ -42,13 +62,17 @@ cr
 	2dup		\ item-size array-addr n-i array-addr n-i
 	stack-new	\ item-size array-addr n-i array-addr stack-addr ( stack allocated )
 	swap		\ item-size array-addr n-i stack-addr array-addr 
-	!		\ item-size array-addr n-i ( stack-addr stored in first array cell )
+	_mm-set-stack	\ item-size array-addr n-i ( stack-addr stored in first array cell )
 
 	rot		\ array-addr (end-state) n-i item-size
 	0		\ array-addr n-i item-size 0
 	2over	drop	\ array-addr n-i item-size 0 array-addr
 	nip		\ array-addr n-i item-size array-addr
-	cell +		\ array-addr n-i item-size array-addr+ ( addr after stack pointer )
+
+	2dup		\ array-addr n-i item-size array-addr item-size array-addr
+	_mm-set-item-size	\ array-addr n-i item-size array-addr
+
+	2 cells +	\ array-addr n-i item-size array-addr+ ( addr after stack pointer, item size )
 
         rot		\ array-addr item-size array-addr+ n-i
 
@@ -65,29 +89,50 @@ cr
 
 	2drop		\ array-addr
 ;
-cr
 
 \ .mma-usage. Run like: "<mma-name> .mma-usage"
 : .mma-usage ( mma-addr -- )
-    @			\ stack-addr
+    dup			\ mma-addr mma-addr
+    _mm-get-stack	\ mma-addr stack-addr
     ." Capacity:"
     space
-    dup			\ stack-addr stack-addr
-    _stack-get-capacity	\ stack-addr capacity
-    dup			\ stack-addr capacity capacity
-    1 .r			\ stack-addr capacity (emit capacity)
-    44 emit		\ stack-addr capacity (emit comma)
+    dup			\ mma-addr stack-addr stack-addr
+    _stack-get-capacity	\ mma-addr stack-addr capacity
+    dup			\ mma-addr stack-addr capacity capacity
+    1 .r		\ mma-addr stack-addr capacity (emit capacity)
+    44 emit		\ mma-addr stack-addr capacity (emit comma)
     space
     ." Free:"
     space
-    swap		\ capacity stack-addr
-   _stack-get-num-free	\ capacity num-free
-    dup			\ capacity num-free num-free
-    1 .r			\ capacity num-free (emit num-free)
-    44 emit		\ (emit comma)
+    dup rot		\ mma-addr capacity capacity stack-addr 
+   _stack-get-num-free	\ mma-addr capacity capacity num-free
+    dup			\ mma-addr capacity capacity num-free num-free
+    1 .r		\ mma-addr capacity capacity num-free
+    44 emit		\ mma-addr capacity capacity num-free
     space
    ." In use:"
    space
-   - . 
+   - . 			\ mma-addr capacity
+   swap			\ capacity mma-addr
+   space
+   ." Item Size:" space
+   _mm-get-item-size	\ capacity item-size
+   dup			\ capacity item-size item-size
+   .			\ capacity item-size
+   over *		\ capacity array-size
+   tuck			\ array-size capacity array-size
+   ." Array size:" space
+   .			\ array-size capacity
+   3 +			\ array-size capacity-cells-in-stack-plust-3-cells-overhead 
+   cells		\ array-size overhead-size
+   dup			\ array-size overhead-size overhead-size
+   ." Overhead:" space
+   .			\ array-size overhead-size
+   ." Total:" space
+   + dup		\ total-size total-size
+   .			\ total-size
+   cell /		\ number cells
+   ." Cells:" space
+   .			\ -- )
 ;
 
