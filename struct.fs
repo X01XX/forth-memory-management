e7006bffed45d803e0752de3ea3d235291957f95
Earl Dukerschein <earl.dukerschein@wisc.edu>
2026-01-03 10:28:27 -0600

    Add files via upload
    Added two functions to struct.fs.
    struct-one-free-deallocate is for the case where a passed argument starts a loop, only to be replaced in the next cycle with a modified version that should be deallocated. 
    !struct stores a struct and increments its use count.

--- struct.fs
+++ struct.fs
@@ -35,3 +35,17 @@
     swap struct-set-use-count
 ;
 
+: struct-one-free-deallocate ( struct-addr -- ) \ Guarantee that a struct will survive one extra deallocation.
+    dup struct-inc-use-count        \ sa
+    dup struct-get-use-count        \ sa uc
+    1 = if
+        struct-inc-use-count
+    else
+        drop
+    then
+;
+
+: !struct ( struct addr -- )    \ Store a struct at a given address.
+    over struct-inc-use-count
+    !
+;
